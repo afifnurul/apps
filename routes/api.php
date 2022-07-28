0a8d1dd08fe1fc29950daeac2033b4fd5962eb1f
afif nurul <afifnurul64@gmail.com>
2022-07-28 18:17:56 +0700

    api-wbhook

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\MidtransNotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// menangani update status transaksi ketika midtrans kirim konfirmasi
+
+Route::post('/notification', [MidtransNotificationController::class, 'handler']);
